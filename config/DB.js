0768ac35f5da415edd917bf665e173f8a8da1484
Shayanrava <shayanravakhah57@gmail.com>
2026-02-09 17:31:22 +0330

    go

--- config/DB.js
+++ config/DB.js
@@ -1,12 +1,23 @@
 import { Sequelize } from "sequelize";
 
 
-const db = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
-  dialect: "mysql",
-  dialectOptions: {
-    ssl: { rejectUnauthorized: false }
-  },
-  logging: false
-});
+const db = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQL_ROOT_PASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: "mysql",
+  }
+);
+
+// const db = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
+//   dialect: "mysql",
+//   dialectOptions: {
+//     ssl: { rejectUnauthorized: false }
+//   },
+//   logging: false
+// });
 
 export default db;
